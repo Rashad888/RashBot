b82b8dce99c3665c886e2ee44f36f102ef65db61
Rashad888 <38947724+Rashad888@users.noreply.github.com>
2020-04-05 01:30:18 +0300

    update

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import botSet
 bot = telebot.TeleBot(botSet.tokken)
 
 @bot.message_handler(content_types=['text'])
-
 def gmsg(message):
     if message.text == "Привет":
         bot.send_message(message.from_user.id,"Как дела?)")
+
+@bot.message_handler(content_types=['vkurl'])
+def vkur(message):
+    keyboardmain = telebot.types.InlineKeyboardMarkup(row_width=2)
+    fbtn = telebot.types.InlineKeyboardButton(text='https://vk.com/rash_ad')
+    keyboardmain.add(fbtn)
+    bot.send_message(message.from_user.id,'Ссылка на страницу Рашада',reply_markup=keyboardmain)
 
 bot.polling(none_stop=True , interval= 0)
