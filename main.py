e0d0a88643a7a26f40c7b8a1504297a4b41f3976
Rashad888 <38947724+Rashad888@users.noreply.github.com>
2020-04-06 19:46:04 +0300

    newversino

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import telebot
 import botSet
 bot = telebot.TeleBot(botSet.tokken)
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['sendmemes'])
 def vkur(message):
-    if message.text == "Что делаешь":
-        bot.send_message(message.from_user.id,"vk.com")
+    bot.send_message(message.from_user.id,"vk.com")
 
 bot.polling(none_stop=True , interval= 0)
