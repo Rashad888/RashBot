e0530aa5e846714cafa9dc73d01e517b23ca6ef2
Rashad888 <38947724+Rashad888@users.noreply.github.com>
2020-04-06 19:28:30 +0300

    asasa

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot(botSet.tokken)
 
 @bot.message_handler(content_types=['text'])
 def vkur(message):
-    if message.text == "/sendmemes":
+    if message.text == "Что делаешь":
         bot.send_message(message.from_user.id,"vk.com")
 
 bot.polling(none_stop=True , interval= 0)
