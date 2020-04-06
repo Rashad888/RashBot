7b6ca0dceec25369dcec30de9fd62f7cecde4dfc
Rashad888 <38947724+Rashad888@users.noreply.github.com>
2020-04-06 21:27:16 +0300

    saas

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot(botSet.tokken)
 
 @bot.message_handler(commands=['start'])
 def welMsg(message):
-    bot.send_sticker(message.chat.id,"CAACAgIAAxkBAAI-9F6LZzJORuLw_RCmageqFRrtNvDEAAIOAAPSfWEYkBysKo8rSV0YBA")
-    bot.send_message(message.chat.id,'Привет! Я бот Рашада, и я буду делать все что ты скажешь!')
+    bot.reply_to(message,"Привет!"+str(bot.send_sticker(message.chat.id,"CAACAgIAAxkBAAI-9F6LZzJORuLw_RCmageqFRrtNvDEAAIOAAPSfWEYkBysKo8rSV0YBA")))
+    #bot.send_sticker(message.chat.id,"CAACAgIAAxkBAAI-9F6LZzJORuLw_RCmageqFRrtNvDEAAIOAAPSfWEYkBysKo8rSV0YBA")
 
 bot.polling(none_stop=True , interval= 0)
