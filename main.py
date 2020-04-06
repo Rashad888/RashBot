9d0bdc3cf0ecba380bd8c8016bf72acfce222b0b
Rashad888 <38947724+Rashad888@users.noreply.github.com>
2020-04-06 21:24:36 +0300

    обнова

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ bot = telebot.TeleBot(botSet.tokken)
 
 @bot.message_handler(commands=['start'])
 def welMsg(message):
-    bot.reply_to(message,"Салам")
     bot.send_sticker(message.chat.id,"CAACAgIAAxkBAAI-9F6LZzJORuLw_RCmageqFRrtNvDEAAIOAAPSfWEYkBysKo8rSV0YBA")
-
+    bot.send_message(message.chat.id,'Привет! Я бот Рашада, и я буду делать все что ты скажешь!')
 
 bot.polling(none_stop=True , interval= 0)
