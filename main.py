297fa3124fa0de7f75a56e5f9bab5b77cda149ef
Rashad888 <38947724+Rashad888@users.noreply.github.com>
2020-04-06 18:41:41 +0300

    n

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import telebot
 import botSet
 bot = telebot.TeleBot(botSet.tokken)
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(content_types=['text'])
 def vkur(message):
     if message.text == "/sendmemes":
         keyboardmain = telebot.types.InlineKeyboardMarkup(row_width=2)
         fbtn = telebot.types.InlineKeyboardButton(text='https://vk.com/rash_ad')
         keyboardmain.add(fbtn)
         bot.send_message(message.from_user.id,'Ссылка на страницу Рашада',reply_markup=keyboardmain)
-
+@bot.message_handler(func = lambda message : True)
+def echoS(message):
+    bot.reply_to(message,message.text)
 bot.polling(none_stop=True , interval= 0)
